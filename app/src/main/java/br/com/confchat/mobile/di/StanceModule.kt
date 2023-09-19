e56f27e8328314a32339a8907321cc98a1508bd2
francivaldo4334 <francivaldodev@gmail.com>
2023-09-19 03:17:15 -0300

    fix: rota base

--- app/src/main/java/br/com/confchat/mobile/di/StanceModule.kt
+++ app/src/main/java/br/com/confchat/mobile/di/StanceModule.kt
@@ -25,8 +25,8 @@ import javax.inject.Singleton
 @Module
 @InstallIn(SingletonComponent::class)
 object StanceModule {
-//    private val BASE_URL="http://52.67.244.32/"
-    private val BASE_URL="http://192.168.0.9:8080/"
+    private val BASE_URL="http://52.67.244.32/"
+//    private val BASE_URL="http://192.168.0.9:8080/"
     private val authInterceptor = Interceptor{chain->
         val request = chain.request();
         val newRequest =
